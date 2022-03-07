5fcacef799be6a93e29d25b140f82df973b3efcd
Ksenyia Kravchuk <kkravchuk@uma.tech>
2022-03-08 01:55:40 +0300

    Add filtration and pagination

--- cryptonomicon/src/App.vue
+++ cryptonomicon/src/App.vue
@@ -35,9 +35,7 @@
         <button
             @click="addTicker"
             type="button"
-            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-        >
-          <!-- Heroicon name: solid/mail -->
+            class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
           <svg
               class="-ml-0.5 mr-2 h-6 w-6"
               xmlns="http://www.w3.org/2000/svg"
@@ -54,10 +52,28 @@
       </section>
 
       <template v-if="tickers.length">
+        <div>Фильтр:
+          <input
+              v-model="filter"
+            class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+            type="text">
+        </div>
+        <button
+            v-if = "page > 1"
+            @click = "page = page - 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          Назад
+        </button>
+        <button
+            v-if = "hasNextPage"
+            @click = "page = page + 1"
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          Вперед
+        </button>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-              v-for="(tick, inx) in tickers"
+              v-for="(tick, inx) in filteredTickers()"
               :key="inx"
               :class="{'border-4': selected === tick}"
               @click="select(tick)"
@@ -107,8 +123,6 @@
             class="absolute top-0 right-0">
           <svg
               xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              xmlns:svgjs="http://svgjs.com/svgjs"
               version="1.1"
               width="30"
               height="30"
@@ -141,10 +155,31 @@ export default {
       ticker: '',
       tickers: [],
       selected: null,
-      graph: []
+      graph: [],
+      page: 1,
+      filter: '',
+      hasNextPage: true
+    }
+  },
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    },
+    page() {
+      this.page = 1;
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
     }
   },
   created() {
+    const windowData = Object.fromEntries(new URL(window.location).searchParams.entries());
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickersData = localStorage.getItem("cryptonomicon-list");
     if (tickersData) {
       this.tickers = JSON.parse(tickersData);
@@ -152,7 +187,6 @@ export default {
         this.subscribeToUpdates(ticker.name)
       })
     }
-
   },
   methods: {
     subscribeToUpdates(tickerName) {
@@ -172,6 +206,18 @@ export default {
       localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
       this.subscribeToUpdates(newTicker.name)
       this.ticker = '';
+      this.filter = '';
+    },
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+
+      const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.filter))
+
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
     },
     handleDelete(tick) {
       this.tickers = this.tickers.filter(t => t !== tick);
