e8f8f99c69aae04e99a43e8271cd29b650735f36
Ksenyia Kravchuk <kkravchuk@uma.tech>
2022-03-08 03:38:14 +0300

    Move logic to computed and watch

--- cryptonomicon/src/App.vue
+++ cryptonomicon/src/App.vue
@@ -73,7 +73,7 @@
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-              v-for="(tick, inx) in filteredTickers()"
+              v-for="(tick, inx) in paginatedTickers"
               :key="inx"
               :class="{'border-4': selected === tick}"
               @click="select(tick)"
@@ -112,7 +112,7 @@
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
-              v-for="(bar, idx) in normalizeGraph()"
+              v-for="(bar, idx) in normalizedGraph"
               :key="idx"
               :style="{ height: `${bar}%`}"
               class="bg-purple-800 border w-10"></div>
@@ -157,18 +157,26 @@ export default {
       selected: null,
       graph: [],
       page: 1,
-      filter: '',
-      hasNextPage: true
+      filter: ''
     }
   },
   watch: {
+    tickers() {
+      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+    },
+    selected() {
+      this.graph = [];
+    },
+    paginatedTickers() {
+      if (this.paginatedTickers.length === 0 && this.page > 1) {
+        this.page -= 1;
+      }
+    },
     filter() {
       this.page = 1;
-      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
     },
-    page() {
-      this.page = 1;
-      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    pageStateOptions(value) {
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${value.filter}&page=${value.page}`)
     }
   },
   created() {
@@ -188,6 +196,40 @@ export default {
       })
     }
   },
+  computed: {
+    startIndex() {
+      return (this.page - 1) * 6;
+    },
+    endIndex() {
+      return this.page * 6;
+    },
+    filteredTickers() {
+      return this.tickers.filter(ticker => ticker.name.includes(this.filter))
+    },
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex;
+    },
+    pageStateOptions() {
+      return {
+        filter: this.filter,
+        page: this.page
+      }
+    },
+    normalizedGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+
+      if (maxValue === minValue) {
+        return this.graph.map(() => 50)
+      }
+      return this.graph.map(
+          price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      )
+    }
+  },
   methods: {
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
@@ -202,37 +244,19 @@ export default {
     },
     addTicker() {
       const newTicker = {name: this.ticker, price: '-'};
-      this.tickers.push(newTicker);
-      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
+      this.tickers = [...this.tickers, newTicker];
       this.subscribeToUpdates(newTicker.name)
       this.ticker = '';
       this.filter = '';
     },
-    filteredTickers() {
-      const start = (this.page - 1) * 6;
-      const end = this.page * 6;
-
-
-      const filteredTickers = this.tickers.filter(ticker => ticker.name.includes(this.filter))
-
-      this.hasNextPage = filteredTickers.length > end;
-
-      return filteredTickers.slice(start, end);
-    },
     handleDelete(tick) {
       this.tickers = this.tickers.filter(t => t !== tick);
-      localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
-    },
-    normalizeGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-      return this.graph.map(
-          price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      )
+      if (this.selected === tick) {
+        this.selected = null;
+      }
     },
     select(ticker) {
       this.selected = ticker;
-      this.graph = [];
     }
   }
 }
